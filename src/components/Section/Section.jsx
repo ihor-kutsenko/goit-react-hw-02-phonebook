8b0c7492e51dcc5a5d687ec6114bd16fc193b230
Rammbest <igor-kutsenko@ukr.net>
2023-07-21 01:29:03 +0300

    add animation for title

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -4,7 +4,9 @@ import css from './Section.module.css';
 const Section = ({ title, children }) => {
   return (
     <section className={css.section}>
-      <h1 title={title}>{title}</h1>
+      <h1 title={title} className={css.title}>
+        {title}
+      </h1>
       {children}
     </section>
   );
